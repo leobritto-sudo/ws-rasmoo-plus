139a9b8ed25c07da1ffedada7139a39e3defa9a7
Leonardo Alves <littocraft@gmail.com>
2023-05-29 10:18:15 -0300

    annotation repository

--- src/main/java/com/client/ws/rasmooplus/repository/UserRepository.java
+++ src/main/java/com/client/ws/rasmooplus/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.client.ws.rasmooplus.repository;
 
 import com.client.ws.rasmooplus.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
