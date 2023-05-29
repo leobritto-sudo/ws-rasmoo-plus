e3042dc59ffdac9b40d1465b7e2a314612d8ba07
Leonardo Alves <littocraft@gmail.com>
2023-05-29 10:55:37 -0300

    criação service

---
+++ src/main/java/com/client/ws/rasmooplus/service/impl/SubscriptionTypeServiceImpl.java
@@ -0,0 +1,40 @@
+package com.client.ws.rasmooplus.service.impl;
+
+import com.client.ws.rasmooplus.model.SubscriptionType;
+import com.client.ws.rasmooplus.repository.SubscriptionTypeRepository;
+import com.client.ws.rasmooplus.service.SubscriptionTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SubscriptionTypeServiceImpl implements SubscriptionTypeService {
+    @Autowired
+    private SubscriptionTypeRepository subscriptionTypeRepository;
+
+    @Override
+    public List<SubscriptionType> findAll() {
+        return subscriptionTypeRepository.findAll();
+    }
+
+    @Override
+    public SubscriptionType findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public SubscriptionType create(SubscriptionType subscriptionType) {
+        return null;
+    }
+
+    @Override
+    public SubscriptionType update(Long id, SubscriptionType subscriptionType) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+}
