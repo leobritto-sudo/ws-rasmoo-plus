73f6ab3d6a2a294f8f84c34e9b0baca9aee07306
Leonardo Alves <littocraft@gmail.com>
2023-05-29 10:14:21 -0300

    criação repositories

---
+++ src/main/java/com/client/ws/rasmooplus/repository/UserPaymentInfoRepository.java
@@ -0,0 +1,7 @@
+package com.client.ws.rasmooplus.repository;
+
+import com.client.ws.rasmooplus.model.UserPaymentInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserPaymentInfoRepository extends JpaRepository<UserPaymentInfo, Long> {
+}
