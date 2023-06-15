84ecffc5cdcb852b52288695d01090729b0e0b3c
Leonardo Alves <littocraft@gmail.com>
2023-06-15 10:52:46 -0300

    find all user type

---
+++ src/main/java/com/client/ws/rasmooplus/service/impl/UserTypeServiceImpl.java
@@ -0,0 +1,28 @@
+package com.client.ws.rasmooplus.service.impl;
+
+import com.client.ws.rasmooplus.exception.NotFoundException;
+import com.client.ws.rasmooplus.model.UserType;
+import com.client.ws.rasmooplus.repository.UserTypeRepository;
+import com.client.ws.rasmooplus.service.UserTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserTypeServiceImpl implements UserTypeService {
+    @Autowired
+    private UserTypeRepository userTypeRepository;
+
+    @Override
+    public List<UserType> findAll() {
+        List<UserType> userType = userTypeRepository.findAll();
+
+        if (userType.isEmpty()) {
+            throw new NotFoundException("Recurso não encontrado");
+        }
+
+        return userType;
+    }
+}
