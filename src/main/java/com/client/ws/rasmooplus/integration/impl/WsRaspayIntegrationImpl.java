3b859669ba46c61e1561f877ff0090d8d20c9594
Leonardo Alves <littocraft@gmail.com>
2023-07-05 10:10:41 -0300

    alcrefatoração http headers

--- src/main/java/com/client/ws/rasmooplus/integration/impl/WsRaspayIntegrationImpl.java
+++ src/main/java/com/client/ws/rasmooplus/integration/impl/WsRaspayIntegrationImpl.java
@@ -4,7 +4,9 @@ import com.client.ws.rasmooplus.dto.wsraspay.CustomerDTO;
 import com.client.ws.rasmooplus.dto.wsraspay.OrderDTO;
 import com.client.ws.rasmooplus.dto.wsraspay.PaymentDTO;
 import com.client.ws.rasmooplus.integration.WsRaspayIntegration;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -12,16 +14,18 @@ import org.springframework.web.client.RestTemplate;
 
 @Component
 public class WsRaspayIntegrationImpl implements WsRaspayIntegration {
-    private RestTemplate restTemplate;
+    private final RestTemplate restTemplate;
 
+    private final HttpHeaders headers;
     public WsRaspayIntegrationImpl() {
         restTemplate = new RestTemplate();
+        headers = getHttpHeaders();
     }
 
     @Override
     public CustomerDTO createCustomer(CustomerDTO customerDTO) {
         try {
-            HttpEntity<CustomerDTO> request = new HttpEntity<>(customerDTO);
+            HttpEntity<CustomerDTO> request = new HttpEntity<>(customerDTO, this.headers);
             ResponseEntity<CustomerDTO> response =
                     restTemplate.exchange("http://localhost:8081/ws-raspay/v1/customer", HttpMethod.POST, request, CustomerDTO.class);
 
@@ -39,5 +43,13 @@ public class WsRaspayIntegrationImpl implements WsRaspayIntegration {
     @Override
     public Boolean processPayment(PaymentDTO paymentDTO) {
         return null;
+    }
+
+    private static HttpHeaders getHttpHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        String credentials = "rasmooplus:r@sm00";
+        String base64 = Base64.encodeBase64String(credentials.getBytes());
+        headers.add("Authorization", "Basic " + base64);
+        return headers;
     }
 }
