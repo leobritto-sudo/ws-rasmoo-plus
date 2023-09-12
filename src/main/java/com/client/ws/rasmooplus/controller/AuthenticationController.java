62ca8f3c589a5f1465c516c9aab04b3ff1bf3b45
Leonardo Alves <littocraft@gmail.com>
2023-09-12 10:37:56 -0300

    refatoração token dto

--- src/main/java/com/client/ws/rasmooplus/controller/AuthenticationController.java
+++ src/main/java/com/client/ws/rasmooplus/controller/AuthenticationController.java
@@ -1,15 +1,12 @@
 package com.client.ws.rasmooplus.controller;
 
 import com.client.ws.rasmooplus.dto.LoginDto;
-import com.client.ws.rasmooplus.exception.BadRequestException;
-import com.client.ws.rasmooplus.service.TokenService;
+import com.client.ws.rasmooplus.dto.TokenDto;
+import com.client.ws.rasmooplus.service.AuthenticationService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,22 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class AuthenticationController {
 
-    @Autowired private AuthenticationManager authenticationManager;
-
-    @Autowired private TokenService tokenService;
+    @Autowired private AuthenticationService authenticationService;
 
     @PostMapping
-    public ResponseEntity<String> auth(@RequestBody @Valid LoginDto loginDto) {
-        UsernamePasswordAuthenticationToken userPassToken =
-                new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword());
-
-        try {
-            Authentication authentication = authenticationManager.authenticate(userPassToken);
-            String token = tokenService.getToken(authentication);
-
-            return ResponseEntity.status(HttpStatus.OK).body(token);
-        } catch (Exception e) {
-            throw new BadRequestException("Erro ao  formatar token - " + e.getMessage());
-        }
+    public ResponseEntity<TokenDto> auth(@RequestBody @Valid LoginDto loginDto) {
+        return ResponseEntity.status(HttpStatus.OK).body(authenticationService.auth(loginDto));
     }
 }
