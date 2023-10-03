8b2f56aafdfc8b5f163dcc2c168fc1c5c7a44e1a
Leonardo Alves <littocraft@gmail.com>
2023-10-03 10:53:45 -0300

    inserindo cacheable

--- src/main/java/com/client/ws/rasmooplus/controller/SubscriptionTypeController.java
+++ src/main/java/com/client/ws/rasmooplus/controller/SubscriptionTypeController.java
@@ -5,6 +5,7 @@ import com.client.ws.rasmooplus.model.SubscriptionType;
 import com.client.ws.rasmooplus.service.SubscriptionTypeService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,11 +20,13 @@ public class SubscriptionTypeController {
     private SubscriptionTypeService subscriptionTypeService;
 
     @GetMapping
+    @Cacheable(value = "subscriptionType")
     public ResponseEntity<List<SubscriptionType>> findAll() {
         return ResponseEntity.status(HttpStatus.OK).body(subscriptionTypeService.findAll());
     }
 
     @GetMapping("/{id}")
+    @Cacheable(value = "subscriptionTypeById", key = "#id")
     public ResponseEntity<SubscriptionType> findById(@PathVariable Long id) {
         SubscriptionType subscriptionType = subscriptionTypeService.findById(id);
 
