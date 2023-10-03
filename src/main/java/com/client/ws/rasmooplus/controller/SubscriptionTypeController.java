918d739b5614c7aa142d579af033192164241152
Leonardo Alves <littocraft@gmail.com>
2023-10-03 11:08:13 -0300

    inserindo cache evict

--- src/main/java/com/client/ws/rasmooplus/controller/SubscriptionTypeController.java
+++ src/main/java/com/client/ws/rasmooplus/controller/SubscriptionTypeController.java
@@ -5,6 +5,7 @@ import com.client.ws.rasmooplus.model.SubscriptionType;
 import com.client.ws.rasmooplus.service.SubscriptionTypeService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,7 +27,7 @@ public class SubscriptionTypeController {
     }
 
     @GetMapping("/{id}")
-    @Cacheable(value = "subscriptionTypeById", key = "#id")
+    @Cacheable(value = "subscriptionType", key = "#id")
     public ResponseEntity<SubscriptionType> findById(@PathVariable Long id) {
         SubscriptionType subscriptionType = subscriptionTypeService.findById(id);
 
@@ -34,16 +35,19 @@ public class SubscriptionTypeController {
     }
 
     @PostMapping
+    @CacheEvict(value = "subscriptionType", allEntries = true)
     public ResponseEntity<SubscriptionType> create(@Valid @RequestBody SubscriptionTypeDTO subscriptionTypeDTO) {
         return ResponseEntity.status(HttpStatus.CREATED).body(subscriptionTypeService.create(subscriptionTypeDTO));
     }
 
     @PutMapping("/{id}")
+    @CacheEvict(value = "subscriptionType", allEntries = true)
     public ResponseEntity<SubscriptionType> update(@PathVariable("id") Long id, @RequestBody SubscriptionTypeDTO subscriptionTypeDTO) {
         return ResponseEntity.status(HttpStatus.OK).body(subscriptionTypeService.update(id, subscriptionTypeDTO));
     }
 
     @DeleteMapping("/{id}")
+    @CacheEvict(value = "subscriptionType", allEntries = true)
     public ResponseEntity<Void> update(@PathVariable("id") Long id) {
         subscriptionTypeService.delete(id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
