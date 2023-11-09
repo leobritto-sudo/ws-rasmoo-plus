dda10e99bf14a1b99722cb517b60d86fcb3e2a32
Leonardo Alves <littocraft@gmail.com>
2023-11-09 11:17:55 -0300

    creating recovery code validation controller

--- src/main/java/com/client/ws/rasmooplus/controller/AuthenticationController.java
+++ src/main/java/com/client/ws/rasmooplus/controller/AuthenticationController.java
@@ -9,10 +9,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
@@ -31,5 +28,11 @@ public class AuthenticationController {
     public ResponseEntity<Void> sendRecoveryCode(@RequestBody @Valid UserRecoveryCode userRecoveryCode) {
         userDetailsService.sendRecoveryCode(userRecoveryCode.getEmail());
         return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
+    }
+
+    @GetMapping("/recovery-code/")
+    public ResponseEntity<Boolean> recoveryCodeValidation(
+            @RequestParam("recoveryCode") String code, @RequestParam("email") String email) {
+        return ResponseEntity.status(HttpStatus.OK).body(userDetailsService.recoveryCodeIsValid(code, email));
     }
 }
