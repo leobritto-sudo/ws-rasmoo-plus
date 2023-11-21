c3447317d2a8ee148184ea3b63711c5318a773a4
Leonardo Alves <littocraft@gmail.com>
2023-11-21 11:04:50 -0300

    unit test recovery code is valid

--- src/test/java/com/client/ws/rasmooplus/service/impl/UserDetailsServiceTest.java
+++ src/test/java/com/client/ws/rasmooplus/service/impl/UserDetailsServiceTest.java
@@ -12,11 +12,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -38,6 +39,9 @@ class UserDetailsServiceTest {
 
     private UserRecoveryCode userRecoveryCode;
 
+    private final static String USERNAME_VALID = "rasmoo@gmail.com";
+    private final static String USERNAME_NOT_VALID = "roger12@gmail.com";
+
     @BeforeEach
     void setup() {
         userCredentials = UserCredentials.builder()
@@ -50,64 +54,96 @@ class UserDetailsServiceTest {
                 .id("wigdnjvi0203")
                 .code("1234")
                 .email("rasmoo@gmail.com")
+                .creationDate(LocalDateTime.now())
                 .build();
     }
 
     //Testes loadByUsername
     @Test
     void givenLoadUserByUsernameWhenUserCredentialsFoundThenReturnUserCredentials() {
-        when(userDetailsRepository.findByUsername("rasmoo@gmail.com")).thenReturn(Optional.of(userCredentials));
+        when(userDetailsRepository.findByUsername(USERNAME_VALID)).thenReturn(Optional.of(userCredentials));
 
-        assertEquals(userCredentials, userDetailsService.loadUserByUsername("rasmoo@gmail.com"));
+        assertEquals(userCredentials, userDetailsService.loadUserByUsername(USERNAME_VALID));
 
-        verify(userDetailsRepository, times(1)).findByUsername("rasmoo@gmail.com");
+        verify(userDetailsRepository, times(1)).findByUsername(USERNAME_VALID);
     }
 
     @Test
     void givenLoadUserByUsernameWhenUserCredentialsNotFoundThenThrowNotFoundException() {
-        when(userDetailsRepository.findByUsername("roger12@gmail.com")).thenReturn(Optional.empty());
+        when(userDetailsRepository.findByUsername(USERNAME_NOT_VALID)).thenReturn(Optional.empty());
 
-        assertThrows(NotFoundException.class, () -> userDetailsService.loadUserByUsername("roger12@gmail.com"));
+        assertThrows(NotFoundException.class, () -> userDetailsService.loadUserByUsername(USERNAME_NOT_VALID));
 
-        verify(userDetailsRepository, times(1)).findByUsername("roger12@gmail.com");
+        verify(userDetailsRepository, times(1)).findByUsername(USERNAME_NOT_VALID);
     }
 
     // Testes sendRecoveryCode
     @Test
     void givenSendRecoveryCodeWhenRecoveryCodeFoundThenSaveNewRecoveryCode() {
-        when(userRecoveryCodeRepository.findByEmail("rasmoo@gmail.com")).thenReturn(Optional.of(userRecoveryCode));
+        when(userRecoveryCodeRepository.findByEmail(USERNAME_VALID)).thenReturn(Optional.of(userRecoveryCode));
 
-        userDetailsService.sendRecoveryCode("rasmoo@gmail.com");
+        userDetailsService.sendRecoveryCode(USERNAME_VALID);
 
-        verify(userRecoveryCodeRepository, times(1)).findByEmail("rasmoo@gmail.com");
-        verify(userDetailsRepository, times(0)).findByUsername("rasmoo@gmail.com");
-        verify(mailIntegration, times(1)).send(eq("rasmoo@gmail.com"), anyString(), anyString());
+        verify(userRecoveryCodeRepository, times(1)).findByEmail(USERNAME_VALID);
+        verify(userDetailsRepository, times(0)).findByUsername(USERNAME_VALID);
+        verify(mailIntegration, times(1)).send(eq(USERNAME_VALID), anyString(), anyString());
         verify(userRecoveryCodeRepository, times(1)).save(userRecoveryCode);
     }
 
     @Test
     void givenSendRecoveryCodeWhenRecoveryCodeNotFoundAndUserCredentialsFoundThenSaveNewRecoveryCode() {
-        when(userRecoveryCodeRepository.findByEmail("rasmoo@gmail.com")).thenReturn(Optional.empty());
-        when(userDetailsRepository.findByUsername("rasmoo@gmail.com")).thenReturn(Optional.of(userCredentials));
+        when(userRecoveryCodeRepository.findByEmail(USERNAME_VALID)).thenReturn(Optional.empty());
+        when(userDetailsRepository.findByUsername(USERNAME_VALID)).thenReturn(Optional.of(userCredentials));
 
-        userDetailsService.sendRecoveryCode("rasmoo@gmail.com");
+        userDetailsService.sendRecoveryCode(USERNAME_VALID);
 
-        verify(userRecoveryCodeRepository, times(1)).findByEmail("rasmoo@gmail.com");
-        verify(userDetailsRepository, times(1)).findByUsername("rasmoo@gmail.com");
-        verify(mailIntegration, times(1)).send(eq("rasmoo@gmail.com"), anyString(), anyString());
+        verify(userRecoveryCodeRepository, times(1)).findByEmail(USERNAME_VALID);
+        verify(userDetailsRepository, times(1)).findByUsername(USERNAME_VALID);
+        verify(mailIntegration, times(1)).send(eq(USERNAME_VALID), anyString(), anyString());
         verify(userRecoveryCodeRepository, times(1)).save(any(UserRecoveryCode.class));
     }
 
     @Test
     void givenSendRecoveryCodeWhenRecoveryCodeNotFoundAndUserCredentialsNotFoundThenThrowNotFoundException() {
-        when(userRecoveryCodeRepository.findByEmail("roger12@gmail.com")).thenReturn(Optional.empty());
-        when(userDetailsRepository.findByUsername("roger12@gmail.com")).thenReturn(Optional.empty());
+        when(userRecoveryCodeRepository.findByEmail(USERNAME_NOT_VALID)).thenReturn(Optional.empty());
+        when(userDetailsRepository.findByUsername(USERNAME_NOT_VALID)).thenReturn(Optional.empty());
 
-        assertThrows(NotFoundException.class, () -> userDetailsService.sendRecoveryCode("roger12@gmail.com"));
+        assertThrows(NotFoundException.class, () -> userDetailsService.sendRecoveryCode(USERNAME_NOT_VALID));
 
-        verify(userRecoveryCodeRepository, times(1)).findByEmail("roger12@gmail.com");
-        verify(userDetailsRepository, times(1)).findByUsername("roger12@gmail.com");
-        verify(mailIntegration, times(0)).send(eq("roger12@gmail.com"), anyString(), anyString());
+        verify(userRecoveryCodeRepository, times(1)).findByEmail(USERNAME_NOT_VALID);
+        verify(userDetailsRepository, times(1)).findByUsername(USERNAME_NOT_VALID);
+        verify(mailIntegration, times(0)).send(eq(USERNAME_NOT_VALID), anyString(), anyString());
         verify(userRecoveryCodeRepository, times(0)).save(any(UserRecoveryCode.class));
+    }
+
+    // Testes recoveryCodeIsValid
+    @Test
+    void givenRecoveryCodeIsValidWhenUserFoundThenReturnTrue() {
+        ReflectionTestUtils.setField(userDetailsService, "minutesTimeout", 5);
+        when(userRecoveryCodeRepository.findByEmail(USERNAME_VALID)).thenReturn(Optional.of(userRecoveryCode));
+
+        assertTrue(userDetailsService.recoveryCodeIsValid("1234", USERNAME_VALID));
+
+        verify(userRecoveryCodeRepository, times(1)).findByEmail(USERNAME_VALID);
+    }
+
+    @Test
+    void givenRecoveryCodeIsValidWhenUserFoundAndGotTimeoutThenReturnFalse() {
+        ReflectionTestUtils.setField(userDetailsService, "minutesTimeout", 5);
+        userRecoveryCode.setCreationDate(LocalDateTime.of(2023, 11, 21, 9, 0));
+        when(userRecoveryCodeRepository.findByEmail(USERNAME_VALID)).thenReturn(Optional.of(userRecoveryCode));
+
+        assertFalse(userDetailsService.recoveryCodeIsValid("1234", USERNAME_VALID));
+
+        verify(userRecoveryCodeRepository, times(1)).findByEmail(USERNAME_VALID);
+    }
+
+    @Test
+    void givenRecoveryCodeIsValidWhenUserNotFoundThenThrowNotFoundException() {
+        when(userRecoveryCodeRepository.findByEmail(USERNAME_NOT_VALID)).thenReturn(Optional.empty());
+
+        assertThrows(NotFoundException.class, () -> userDetailsService.recoveryCodeIsValid("1234", USERNAME_NOT_VALID));
+
+        verify(userRecoveryCodeRepository, times(1)).findByEmail(USERNAME_NOT_VALID);
     }
 }
