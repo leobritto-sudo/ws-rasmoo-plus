5462cbb620d46b0551a04719b37a2809c3294eba
Leonardo <leo-britto65@hotmail.com>
2023-12-07 15:27:33 -0300

    creating user type controller test

---
+++ src/test/java/com/client/ws/rasmooplus/controller/UserTypeControllerTest.java
@@ -0,0 +1,52 @@
+package com.client.ws.rasmooplus.controller;
+
+import com.client.ws.rasmooplus.model.jpa.UserType;
+import com.client.ws.rasmooplus.service.UserTypeService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest(UserTypeController.class)
+@AutoConfigureDataJpa
+@AutoConfigureTestDatabase
+@AutoConfigureMockMvc(addFilters = false)
+@ActiveProfiles(profiles = "test")
+class UserTypeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserTypeService userTypeService;
+
+    @Test
+    void givenFindAllThenReturnAllUserTypes() throws Exception {
+
+        List<UserType> userTypeList = new ArrayList<>();
+        UserType userType1 = new UserType(1L, "Professor", "Professor da plataforma");
+        UserType userType2 = new UserType(2L, "Administrador", "Funcionário");
+
+        userTypeList.add(userType1);
+        userTypeList.add(userType2);
+
+        Mockito.when(userTypeService.findAll()).thenReturn(userTypeList);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/user-type"))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+}
